04af737d5da753757ee2a704a875246055ea290d
Joanna Urbiak <joanna.urbiak@o2.pl>
2018-06-03 13:21:33 +0200

    Modify script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 $(function(){
-
   var carouselList = $('#carousel ul');
+  
+  setInterval(changeSlide, 5000);
 
-  setInterval(function() {
-    carouselList.animate({'marginLeft':-400}, 400, function moveFirstSlide() {
-      var firstItem = carouselList.find('li:first');
-      var lastItem = carouselList.find('li:last');
-      lastItem.after(firstItem);
-      carouselList.css({marginLeft:0});
-    });
-  },5000);
+  function changeSlide() {
+    carouselList.animate({'marginLeft':-400}, 2000, moveFirstSlide);;
+  }
+
+  function moveFirstSlide() {
+    var firstItem = carouselList.find('li:first');
+    var lastItem = carouselList.find('li:last');
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  };
 
 });
